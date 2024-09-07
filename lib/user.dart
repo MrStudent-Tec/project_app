ff64dbce2e4a34590f62870a9eb9f6de92763623
MrStudent-Tec <juegosjdrp@gmail.com>
2024-09-07 16:27:37 -0500

    Add files via upload

---
+++ lib/user.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_u/chat.dart';
+import 'package:proyecto_u/comunity.dart';
+import 'package:proyecto_u/group.dart';
+import 'package:proyecto_u/settings.dart';
+
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Usuario',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: const Color(0xFF004D40),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              color: Colors.white,
+              onPressed: () {
+                // Aquí puedes implementar la acción de búsqueda
+                print('Buscar icono presionado');
+              },
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(50.0),
+            child: Container(
+              color: Colors.teal, // Fondo diferente para las pestañas
+              child: const TabBar(
+                indicatorColor: Colors.white,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black54,
+                tabs: [
+                  Tab(text: 'Chats'),
+                  Tab(text: 'Grupos'),
+                  Tab(text: 'Comunidad'),
+                  Tab(text: 'Ajustes'),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ChatScreen(),
+            GroupScreen(),
+            ComunityScreen(),
+            SettingsScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
