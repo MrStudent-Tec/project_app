f89123c8cc14f5741957048ff22609fefb011609
MrStudent-Tec <juegosjdrp@gmail.com>
2024-09-12 11:10:59 -0500

    Update register.php

--- api/register.php
+++ api/register.php
@@ -3,8 +3,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 // Conexión a la base de datos
 $conn = new mysqli('localhost', 'root', '', 'ubook');
+
 
 // Verificar si la conexión fue exitosa
 if ($conn->connect_error) {
@@ -13,13 +15,14 @@ if ($conn->connect_error) {
 
 // Obtener datos del cuerpo de la solicitud POST y validar que no estén vacíos
 $id_usuario = isset($_POST['id_usuario']) ? $_POST['id_usuario'] : '';
+$correo = isset($_POST['correo']) ? $_POST['correo'] : '';
 $nombre_usuario = isset($_POST['nombre_usuario']) ? $_POST['nombre_usuario'] : '';
-$email = isset($_POST['email']) ? $_POST['email'] : '';
 $contrasena = isset($_POST['contrasena']) ? $_POST['contrasena'] : '';
-$fec_nac = isset($_POST['fec_nac']) ? $_POST['fec_nac'] : '';
+$fecha_nac = isset($_POST['fecha_nac']) ? $_POST['fecha_nac'] : '';
 $programa = isset($_POST['programa']) ? $_POST['programa'] : '';
 
-if (empty($id_usuario)|| empty($nombre_usuario) || empty($email)  || empty($contrasena) || empty($fec_nac) || empty($programa)) {
+// Validación de campos vacíos
+if (empty($id_usuario)||empty($correo) || empty($nombre_usuario) || empty($contrasena) || empty($fecha_nac) || empty($programa)) {
     die(json_encode(['success' => false, 'message' => 'Todos los campos son obligatorios']));
 }
 
@@ -27,8 +30,8 @@ if (empty($id_usuario)|| empty($nombre_usuario) || empty($email)  || empty($cont
 $contrasena_hash = password_hash($contrasena, PASSWORD_DEFAULT);
 
 // Preparar la consulta para insertar el nuevo usuario
-$stmt = $conn->prepare("INSERT INTO usuario (id_usuario, nombre_usuario, email, contrasena, fec_nac, programa) VALUES (?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("sss", $id_usuario, $nombre_usuario, $email, $contrasena_hash, $fec_nac, $programa);
+$stmt = $conn->prepare("INSERT INTO usuario (id_usuario, correo, nombre_usuario, contrasena, fecha_nac, programa) VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("isssss",$id_usuario, $correo, $nombre_usuario, $contrasena_hash, $fecha_nac, $programa);
 
 // Ejecutar la consulta y verificar si fue exitosa
 if ($stmt->execute()) {
@@ -41,3 +44,4 @@ if ($stmt->execute()) {
 $stmt->close();
 $conn->close();
 ?>
+
