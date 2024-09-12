368ccb3286a89b9e5f99b1a5c44cb2ac2959be40
MrStudent-Tec <juegosjdrp@gmail.com>
2024-09-12 15:56:18 -0500

    Update register.php

--- api/register.php
+++ api/register.php
@@ -14,14 +14,14 @@ if ($conn->connect_error) {
 
 // Obtener datos del cuerpo de la solicitud POST y validar que no estén vacíos
 $id_usuario = isset($_POST['id_usuario']) ? $_POST['id_usuario'] : '';
-$correo = isset($_POST['correo']) ? $_POST['correo'] : '';
 $nombre_usuario = isset($_POST['nombre_usuario']) ? $_POST['nombre_usuario'] : '';
+$correo = isset($_POST['correo']) ? $_POST['correo'] : '';
 $contrasena = isset($_POST['contrasena']) ? $_POST['contrasena'] : '';
 $fecha_nac = isset($_POST['fecha_nac']) ? $_POST['fecha_nac'] : '';
 $programa = isset($_POST['programa']) ? $_POST['programa'] : '';
 
 // Validación de campos vacíos
-if (empty($id_usuario) || empty($correo) || empty($nombre_usuario) || empty($contrasena) || empty($fecha_nac) || empty($programa)) {
+if (empty($id_usuario) || empty($nombre_usuario) || empty($correo) || empty($contrasena) || empty($fecha_nac) || empty($programa)) {
     die(json_encode(['success' => false, 'message' => 'Todos los campos son obligatorios']));
 }
 
